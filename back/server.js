c16c2c7a1603c71ff3b2ff7166072ffd2f161931
Tristandac28 <tdacier@siclade.fr>
2024-09-25 16:53:03 +0200

    maj

--- back/server.js
+++ back/server.js
@@ -1,36 +1,56 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+        origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],  // Allow both localhost and 127.0.0.1
+        methods: ["GET", "POST"]
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 
+let users = {};
+
+// Fichiers statiques
+app.use(express.static(path.join(__dirname)));
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Guess the Picture Game Server is running');
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
 
+app.get('/game.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'game.html'));
+});
+
+// Gestion des connexions Socket.io
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  // Envoyer une image à deviner
-  socket.emit('newImage', { imageUrl: 'http://example.com/image.jpg' });
+  // Enregistrement du pseudo de l'utilisateur
+  socket.on('setUsername', (username) => {
+    users[socket.id] = username;
+    io.emit('updateUserList', Object.values(users));
+    console.log(`${username} has connected`);
+  });
 
-  // Recevoir une réponse de l'utilisateur
-  socket.on('guess', (data) => {
-    console.log(`User ${socket.id} guessed: ${data.guess}`);
-    // Logique pour vérifier la réponse et envoyer un retour
-    if (data.guess === 'correctAnswer') {
-      socket.emit('result', { correct: true });
-    } else {
-      socket.emit('result', { correct: false });
+  // Envoi de message
+  socket.on('sendMessage', (message) => {
+    if (users[socket.id]) {  // Assure que l'utilisateur est identifié
+        io.emit('newMessage', { user: users[socket.id], message });
     }
   });
 
+  // Déconnexion de l'utilisateur
   socket.on('disconnect', () => {
-    console.log('A user disconnected:', socket.id);
+    console.log(`${users[socket.id]} has disconnected`);
+    delete users[socket.id];
+    io.emit('updateUserList', Object.values(users));
   });
 });
 
